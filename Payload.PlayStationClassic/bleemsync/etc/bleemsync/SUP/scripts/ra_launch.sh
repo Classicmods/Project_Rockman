31a6973a0d52280149791645323d1f75b785ae62
CompCom <33770230+CompCom@users.noreply.github.com>
2019-09-26 17:28:15 +1000

    RetroArch Config Migration.

--- Payload.PlayStationClassic/bleemsync/etc/bleemsync/SUP/scripts/ra_launch.sh
+++ Payload.PlayStationClassic/bleemsync/etc/bleemsync/SUP/scripts/ra_launch.sh
@@ -11,7 +11,9 @@ chmod +x "$retroarch_path/retroarch"
 
 touch "/tmp/launch_ra.flag"
 
+export XDG_CONFIG_HOME="$retroarch_path/config"
+
 while [ -f "/tmp/launch_ra.flag" ]; do
   rm -f "/tmp/launch_ra.flag"
-  PATH="$PATH:$bleemsync_path/bin" "$retroarch_path/retroarch" "$@" 2>&1 | "$bleemsync_path/bin/ra_watch" >> "$runtime_log_path/retroarch.log"
+  PATH="$PATH:$bleemsync_path/bin" "$retroarch_path/retroarch" --config "$retroarch_path/config/retroarch/retroarch.cfg" "$@" 2>&1 | "$bleemsync_path/bin/ra_watch" >> "$runtime_log_path/retroarch.log"
 done
